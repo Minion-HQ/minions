2f95dfc20c0b021ed28a853fe3e6b086176cd9c8
banana-nana <50461810+banana-nana@users.noreply.github.com>
2022-03-23 14:12:04 -0500

    Version 1.1

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,5 @@
+
+
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
@@ -6,11 +8,4 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
-}
-
-function Signin() {
-  window.alert("Welcome Back!");
-}
-function SignOut() {
-  window.alert("Farewell.");
 }
